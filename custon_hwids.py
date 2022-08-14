4570201c3e3ca210d7e14dd47973b003d3c2e9f9
EmiIy <111208076+EmiIy1@users.noreply.github.com>
2022-08-14 01:42:09 +0100

    Typo

--- custon_hwids.py
+++
@@ -1,9 +0,0 @@
-Import("env")
-
-board_config = env.BoardConfig()
-# should be array of VID:PID pairs
-board_config.update("build.hwids", [
-  ["0x1ccf", "0x101c"],
-])
-
-board_config.update("build.usb_product", "YuantCon")
